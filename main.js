a80d297753d74cf55340a66fa2b579d1ed508c5f
Lecogoni <giraud.nicolas@me.com>
2021-10-29 13:47:53 +0000

    WIP

--- main.js
+++ main.js
@@ -3,10 +3,29 @@ let listeNonTriee = [44,55,42,5,10];
 
 let listeTriee = [];
 
+arrayIsSorted = false
+
 // Bubble sort
 
 // https://www.youtube.com/watch?v=lyZQPjUT5B4
 
 
 // implémenter un algorithme de tri à bulle (pas de copy / paste)
+
+
+for(let i = 0; i < listeNonTriee.length; i++ ){
+
+    // check if array is sorted
+
+    while
+
+    let tempActuali = listeNonTriee[i];
+    let tempActualiNext = listeNonTriee[i+1];
+
+    if(listeNonTriee[i] > listeNonTriee[i+1]){
+        tempActuali = listeNonTriee[i+1];
+        tempActualiNext = listeNonTriee[i];
+    }
+
+}
 
