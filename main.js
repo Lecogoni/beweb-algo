2d741741f28aa8a8a24bd8b52b9d356029bb50ed
Lecogoni <giraud.nicolas@me.com>
2021-10-29 10:08:29 +0000

    while loop implementation

--- main.js
+++ main.js
@@ -3,27 +3,25 @@
 
 let liste = [10, 34, 56, 67, 23, 563];
 
-// opération : la boucle for 
+// opération : la boucle while
 
 
-// afficher chaque valuer du tableau sur l'UI
 
-let r ;
-let a = 0 ;
-// afficher chaque valeur du tableau sur l'UI
-r = liste[a]
-console.log(r)
-a++;
-r = liste[a]
-console.log(r)
-a++;
-r = liste[a]
-console.log(r)
+// initialisation de la boucles 
 
-// initialisation de la boucles /2 test de la condition , si la condition est validé on rentre dans la boucle 3/operation entre chaque itération
-for(let i = 0;i < liste.length;i++){
-    let r = liste [i];
-    console.log(" la valeur dans le tableau: " + r );
-    console.log("la valeur de l'index (i) : " + i);
+let i = 0; //1
+
+while(i < liste.length){
+    let r = liste [i]; //2
+    console.log(r); // 2
     console.log("");
+
+    i++ //1
 }
+
+/*
+
+Le code en 1 n'est pas du code métier => c'est du code pour faire fonctionner ma boucle
+le code en 2 est du traitement
+
+*/
