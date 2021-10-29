3b8d6696c5344083c159870948d5d8106bb24a86
Lecogoni <giraud.nicolas@me.com>
2021-10-29 13:37:11 +0000

    set exercice

---
+++ main.js
@@ -0,0 +1,12 @@
+//initialisation des variables
+let listeNonTriee = [44,55,42,5,10];
+
+let listeTriee = [];
+
+// Bubble sort
+
+// https://www.youtube.com/watch?v=lyZQPjUT5B4
+
+
+// implémenter un algorithme de tri à bulle (pas de copy / paste)
+
