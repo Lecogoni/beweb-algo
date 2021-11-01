99207b477dff75b9c30993439623f9a96ec5b838
Lecogoni <giraud.nicolas@me.com>
2021-11-01 20:05:39 +0000

    exo dichotomie search ok

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ let listeTriee = [];
 
 let arrayIsSorted = false;
 
+
+
 // Bubble sort
 
 // https://www.youtube.com/watch?v=lyZQPjUT5B4
@@ -35,6 +37,33 @@ while (arrayIsSorted === false) {
     }
 }
 
+console.log('Arry trié : ');
 console.log(listeNonTriee);
 
+
+
+// implémenter une recherche dichotomique sur une liste ordonnée 
+
+// https://www.youtube.com/watch?v=JdwWMnU04pQ
+
+let sortedArray = [2, 5, 7, 14, 23, 26, 30, 33, 38, 44, 49, 50] 
+let researchValue = 30;
+
+
+let left = 0 
+let right = sortedArray.length - 1; 
+
+while (left <= right) {
+    let middle = parseInt((left + right) / 2) 
+    console.log('middle : ' + middle);
+    if ( sortedArray[middle] === researchValue) {
+        return console.log('in the list');
+    } else if (sortedArray[middle] > researchValue) {
+        right = middle - 1 
+        console.log('right : ' + right);
+    } else {
+        left = middle + 1
+        console.log('left : ' + left);
+    }
+}
 
