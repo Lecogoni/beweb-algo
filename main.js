c8fb4228b05b5698ec1da0463170f4d3ec971b4c
Lecogoni <giraud.nicolas@me.com>
2021-11-02 14:25:47 +0000

    set files to show htlm

--- main.js
+++ main.js
@@ -1,7 +1,60 @@
 // create a chess board fill up with 0 et 1 in a 2D array
 
-let numOfCase = 8;
+// variable declaration zone 
 
-for(let i = 0, i < numOfCase, i++){
-  
+let numOfCase = 8;
+let board = []
+
+let element = document.getElementById(id);
+
+
+// CODE version 1 
+
+for(let i = 0 ; i < numOfCase; i++){
+
+  board[i] = []
+
+  for(let j = 0; j < numOfCase; j++){
+
+    if (i % 2 === 0) {
+
+      if(j % 2 === 0){
+        board[i][j] = 1
+      } else {
+        board[i][j] = 0
+      }
+
+    } else if (i % 2 != 0) {
+      if(j % 2 === 0){
+        board[i][j] = 0
+      } else {
+        board[i][j] = 1
+      }
+    }
+  }
 }
+
+console.log(board);
+
+// CODE version 2 
+
+for(let i = 0 ; i < numOfCase; i++){
+
+  board[i] = []
+  let black = true
+
+  for(let j = 0; j < numOfCase; j++){
+    
+    if (black === true) {
+      board[i][j] = 0
+    } else {
+      board[i][j] = 1
+    }
+    black = !black
+  }
+}
+
+
+console.log('');
+console.log('version 2 : ');
+console.log(board);
